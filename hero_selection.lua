4cfd0b9a9bd4e24d669dce4320d1bf95940a9112
Erik Odenman <erik.odenman@gmail.com>
2016-12-29 01:08:35 +0100

    Fixed hero selection for different player slots

--- hero_selection.lua
+++ hero_selection.lua
@@ -1,43 +1,32 @@
 
-
-----------------------------------------------------------------------------------------------------
+local pickForPlayers = true
+local heroes = {
+  [TEAM_RADIANT] = {
+    "npc_dota_hero_nevermore",
+    "npc_dota_hero_medusa",
+    "npc_dota_hero_lina",
+    "npc_dota_hero_razor",
+    "npc_dota_hero_windrunner"
+  },
+  [TEAM_DIRE] = {
+    "npc_dota_hero_lina",
+    "npc_dota_hero_earthshaker",
+    "npc_dota_hero_nevermore",
+    "npc_dota_hero_mirana",
+    "npc_dota_hero_juggernaut"
+  }
+}
 
 function Think()
-
   function f()
-    
-    if GetGameMode() == GAMEMODE_1V1MID then
-      if GetTeam() == TEAM_RADIANT then
-        SelectHero(0, "npc_dota_hero_nevermore")
-      elseif GetTeam() == TEAM_DIRE then       
-        SelectHero(5, "npc_dota_hero_nevermore")
-      end
-    else
-      if ( GetTeam() == TEAM_RADIANT )
-      then
-        print( "selecting radiant" );
-        SelectHero( 0, "npc_dota_hero_nevermore" );
-        SelectHero( 1, "npc_dota_hero_medusa" );
-        SelectHero( 2, "npc_dota_hero_lina" );
-        SelectHero( 3, "npc_dota_hero_razor" );
-        SelectHero( 4, "npc_dota_hero_windrunner" );
-      elseif ( GetTeam() == TEAM_DIRE )
-      then
-        print( "selecting dire" );
-        SelectHero( 5, "npc_dota_hero_lina" );
-        SelectHero( 6, "npc_dota_hero_earthshaker" );
-        SelectHero( 7, "npc_dota_hero_nevermore" );
-        SelectHero( 8, "npc_dota_hero_mirana" );
-        SelectHero( 9, "npc_dota_hero_juggernaut" );
+    for i,player in ipairs(GetTeamPlayers(GetTeam())) do
+      if pickForPlayers or IsPlayerBot(player) then
+        SelectHero(player, heroes[GetTeam()][i])
       end
     end
   end
-  
   local status, err = pcall(f)
   if not status then
     print(err)
   end
-
 end
-
-----------------------------------------------------------------------------------------------------
