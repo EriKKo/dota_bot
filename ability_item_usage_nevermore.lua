3afff454524dfac63facd7faab57ba9f33451ba4
Erik Odenman <erik.odenman@gmail.com>
2017-01-15 22:54:13 +0100

    Added simple ability leveling code (still waiting for API improvements

--- ability_item_usage_nevermore.lua
+++ ability_item_usage_nevermore.lua
@@ -32,4 +32,62 @@ function AbilityUsageThink()
 
 end
 
+local abilities = {"nevermore_shadowraze1", "nevermore_shadowraze2", "nevermore_shadowraze3", "nevermore_necromastery", "nevermore_dark_lord", "nevermore_requiem", "special_bonus_movement_speed_15", "special_bonus_attack_speed_20", "special_bonus_spell_amplify_6", "special_bonus_hp_175", "special_bonus_evasion_15", "special_bonus_unique_nevermore_1", "special_bonus_attack_range_150", "special_bonus_unique_nevermore_2"}
+local abilityLevelupOrder = {
+  "nevermore_necromastery", 
+  "nevermore_shadowraze1",
+  "nevermore_shadowraze1",
+  "nevermore_necromastery",
+  "nevermore_shadowraze1",
+  "nevermore_necromastery",
+  "nevermore_shadowraze1",
+  "nevermore_necromastery",
+  "nevermore_requiem",
+  "special_bonus_movement_speed_15",
+  "nevermore_dark_lord",
+  "nevermore_requiem",
+  "nevermore_dark_lord",
+  "nevermore_dark_lord",
+  "special_bonus_hp_175",
+  "nevermore_dark_lord",
+  nil,
+  "nevermore_requiem",
+  nil,
+  "special_bonus_unique_nevermore_1",
+  nil,
+  nil,
+  nil,
+  nil,
+  "special_bonus_attack_range_150"
+}
+
+local function GetHeroLevel(bot)
+  local respawnTable = {8, 10, 12, 14, 16, 26, 28, 30, 32, 34, 36, 46, 48, 50, 52, 54, 56, 66, 70, 74, 78,  82, 86, 90, 100};
+  local nRespawnTime = bot:GetRespawnTime() + 1
+  for k,v in pairs (respawnTable) do
+    if v == nRespawnTime then
+      return k
+    end
+  end
+end
+
+function AbilityLevelUpThink()
+  function f()
+    local bot = GetBot()
+    local heroLevel = GetHeroLevel(bot)
+    for i = heroLevel,1,-1 do
+      local abilityName = abilityLevelupOrder[i]
+      if abilityName and bot:GetAbilityByName(abilityName):CanAbilityBeUpgraded() then
+        --print(GetTeam(), "Leveling", abilityName, bot:GetAbilityByName(abilityName):GetLevel())
+        bot:Action_LevelAbility(abilityName)
+        break
+      end
+    end
+  end
+  local status, err = pcall(f)
+  if not status then
+    print(err)
+  end
+end
+
 ---------------------------------------------------------------------------------------------------
